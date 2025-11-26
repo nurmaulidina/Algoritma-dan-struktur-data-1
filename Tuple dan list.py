930e5f598d8117d0c17f558e72b0a851fdc55e92
Mahmuddin Mursalim <mahmuddinmursalim5@gmail.com>
2025-11-27 05:42:28 +0800

    Add files via upload

---
+++ Tuple dan list.py
@@ -0,0 +1,35 @@
+# Mahmuddin Mursalim
+# D0425509
+
+
+# tuple
+
+
+
+negara = ("argentina", "portugal", "italia")	#tuple tidak bisa di ubah
+
+print("isi tuple: " ,negara)
+
+print("Elemen ke-1:" ,negara[0])
+
+# negara[1] = "portugal"	# ini akan ERROR karena tuple tidsk bisa diubah
+
+
+
+
+
+# List
+
+pemain = ["messi", "ronaldo", "platini"]	#bisa diubah
+
+print("sebelum diubah: ", pemain)
+
+
+
+pemain[1] = "platini"	#mengubah isi
+
+pemain.append("neyamar")	#menambah data
+
+
+
+print("setelah diubah: ", pemain)
